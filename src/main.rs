92c7106632c98990fb40177c06f400335dc6cdd7
Jonathan Tran <jonnytran@gmail.com>
2021-01-09 04:04:33 -0500

    Add multiplayer camera
    This also fixes the flicker introduced by the last commit since the
    original camera system was never intended to work with multiple
    cameras.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::convert::TryFrom;
 
-use bevy::{prelude::*, render::camera::OrthographicProjection};
+use bevy::{prelude::*, render::camera::{Camera, CameraProjection, OrthographicProjection}};
 use bevy_tiled_prototype::{TiledMapCenter, TiledMapComponents, TiledMapPlugin};
 
 mod character;
@@ -209,6 +209,52 @@ fn bounding_box(translation: Vec3, size: Vec2) -> Rect<f32> {
     }
 }
 
+// Returns the bounding box that includes both the given bounding boxes.
+fn expand_bounding_box(r1: &Rect<f32>, r2: &Rect<f32>) -> Rect<f32> {
+    Rect {
+        left: r1.left.min(r2.left),
+        right: r1.right.max(r2.right),
+        top: r1.top.max(r2.top),
+        bottom: r1.bottom.min(r2.bottom),
+    }
+}
+
+fn rect_center(r: &Rect<f32>) -> Vec2 {
+    // Don't overflow.
+    Vec2::new(r.left + 0.5 * (r.right - r.left), r.bottom + 0.5 * (r.top - r.bottom))
+}
+
+#[allow(dead_code)]
+fn rect_half_width_height(r: &Rect<f32>) -> Vec2 {
+    Vec2::new(0.5 * (r.right - r.left), 0.5 * (r.top - r.bottom))
+}
+
+fn rect_width_height(r: &Rect<f32>) -> Vec2 {
+    Vec2::new(r.right - r.left, r.top - r.bottom)
+}
+
+fn rect_expand_by(r: &Rect<f32>, amount: f32) -> Rect<f32> {
+    Rect {
+        left: r.left - amount,
+        right: r.right + amount,
+        top: r.top + amount,
+        bottom: r.bottom - amount,
+    }
+}
+
+// wh is width and height.
+// aspect_ratio is the desired width / height.
+fn expanded_to_aspect_ratio(wh: &Vec2, aspect_ratio: f32) -> Vec2 {
+    let h_based_on_w = wh.x / aspect_ratio;
+    if h_based_on_w > wh.y {
+        Vec2::new(wh.x, h_based_on_w)
+    } else {
+        let w_based_on_h = wh.y * aspect_ratio;
+
+        Vec2::new(w_based_on_h, wh.y)
+    }
+}
+
 fn viewport(camera_transform: &GlobalTransform, projection: &OrthographicProjection) -> Rect<f32> {
     let translation = &camera_transform.translation;
     Rect {
@@ -226,27 +272,78 @@ fn is_rect_completely_inside(r1: &Rect<f32>, r2: &Rect<f32>) -> bool {
 }
 
 fn update_camera_system(
+    windows: Res<Windows>,
     mut player_query: Query<&GlobalTransform, With<Player>>,
-    mut camera_query: Query<(&mut Transform, &GlobalTransform, &OrthographicProjection), With<PlayerCamera>>,
+    mut camera_query: Query<(&mut Transform,
+                            &GlobalTransform,
+                            &mut OrthographicProjection,
+                            &mut Camera),
+                            With<PlayerCamera>>,
 ) {
+    // Amount of margin between edge of view and character.
+    let margin = 100.0;
+
+    // Get bounding box of all players.
+    let mut full_bb = None;
+    let mut num_players = 0;
+    let mut player_translation = Vec3::zero();
     for player_transform in player_query.iter_mut() {
+        num_players += 1;
         // Is sprite in view frame?
         // println!("player translation {:?}", player_transform.translation);
         let char_translation = player_transform.translation;
         let char_size = Vec2::new(PLAYER_WIDTH * player_transform.scale.x, PLAYER_HEIGHT * player_transform.scale.y);
         let char_rect = bounding_box(char_translation, char_size);
         // println!("char_rect {:?}", char_rect);
-        for (mut camera_transform, camera_global, projection) in camera_query.iter_mut() {
+        full_bb = match full_bb {
+            None => {
+                player_translation = player_transform.translation;
+                Some(char_rect)
+            }
+            Some(bb) => Some(expand_bounding_box(&bb, &char_rect)),
+        };
+    }
+
+    if let Some(full_bb) = full_bb {
+        let window = windows.get_primary().expect("should be at least one window so we can compute aspect ratio");
+        let win_width = window.width();
+        let win_height = window.height();
+        let aspect_ratio = win_width / win_height;
+
+        // Add margin.
+        // TODO: Handle case when window is smaller than margin.
+        let full_bb = rect_expand_by(&full_bb, margin);
+
+        for (mut camera_transform, camera_global, mut projection, mut camera) in camera_query.iter_mut() {
             // println!("projection {:?}", projection);
             // println!("camera_transform {:?}", camera_transform);
             // println!("camera_global {:?}", camera_global);
             // Note: We don't support camera rotation or scale.
-            let camera_rect = viewport(&camera_global, projection);
+            let camera_rect = viewport(&camera_global, &projection);
             // println!("camera_rect {:?}", camera_rect);
-            let is_player_in_view = is_rect_completely_inside(&char_rect, &camera_rect);
-            if !is_player_in_view {
-                // Mutate the transform, never the global transform.
-                camera_transform.translation = player_transform.translation;
+            if num_players <= 1 {
+                // Center on the player if not in view.
+                let is_player_in_view = is_rect_completely_inside(&full_bb, &camera_rect);
+                if !is_player_in_view {
+                    // Mutate the transform, never the global transform.
+                    camera_transform.translation = player_translation;
+                }
+            } else {
+                // Center on the center of the bounding box of all players.
+                let c = rect_center(&full_bb);
+                camera_transform.translation.x = c.x;
+                camera_transform.translation.y = c.y;
+
+                // Zoom so that all players are in view.
+                let mut wh = rect_width_height(&full_bb);
+                wh = expanded_to_aspect_ratio(&wh, aspect_ratio);
+                // Never zoom in smaller than the window.
+                if wh.x < win_width || wh.y < win_height {
+                    wh = Vec2::new(win_width, win_height);
+                }
+                projection.update(wh.x, wh.y);
+                camera.projection_matrix = projection.get_projection_matrix();
+                camera.depth_calculation = projection.depth_calculation();
             }
         }
     }
