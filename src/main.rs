f72a21f339bda7a1a24e962ef8ee61b7f0503745
mdan <dmtaub@gmail.com>
2021-02-18 23:45:55 -0500

    not removing debuggable, setting visible from query, introduce bug

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,9 @@ pub struct TransientState {
     next_map: Option<Handle<Map>>,
     loaded_maps: HashSet<Handle<Map>>,
     entity_visibility: HashMap<Entity, bool>, // this is a minor memory leak until maps aren't recreated
+
     default_blue: Handle<ColorMaterial>,
+    default_red: Handle<ColorMaterial>,
     button_color: Handle<ColorMaterial>,
     button_hovered_color: Handle<ColorMaterial>,
     button_pressed_color: Handle<ColorMaterial>,
@@ -158,24 +160,20 @@ fn handle_input_system(
     input_actions: Res<InputActionSet>,
     mut transient_state: ResMut<TransientState>,
     mut query: Query<(&mut Character, &Player)>,
-    mut debuggable: Query<&mut Visible, With<Debuggable>>,
     mut dialogue_query: Query<&mut Dialogue>,
+    mut debuggable: Query<(&mut Visible, Option<&Handle<Map>>), With<Debuggable>>,
 ) {
     // check for debug status flag differing from transient_state to determine when to hide/show debug stuff
-    if input_actions.has_flag(Flag::Debug) {
-        if !transient_state.debug_mode {
-            // for now hide, but ideally we spawn debug things here
-            for mut visible in debuggable.iter_mut() {
-                visible.is_visible = true;
-            }
-            transient_state.debug_mode = true;
+    if input_actions.has_flag(Flag::Debug) != transient_state.debug_mode {
+        transient_state.debug_mode = !transient_state.debug_mode;
+        // for now hide, but ideally we spawn debug things here
+        for (mut visible, map_option) in debuggable.iter_mut() {
+            let mut in_current_map = true;
+            map_option.map(|map_handle| {
+                in_current_map = *map_handle == transient_state.current_map;
+            });
+            visible.is_visible = in_current_map && transient_state.debug_mode;
         }
-    } else if transient_state.debug_mode {
-        // for now show
-        for mut visible in debuggable.iter_mut() {
-            visible.is_visible = false;
-        }
-        transient_state.debug_mode = false;
     }
 
     for (mut character, player) in query.iter_mut() {
@@ -249,6 +247,7 @@ fn setup_system(
 ) {
     // Default materials
     let default_blue = materials.add(Color::rgba(0.4, 0.4, 0.9, 0.5).into());
+    let default_red = materials.add(Color::rgba(1.0, 0.4, 0.9, 0.8).into());
     // Cameras.
     commands
         .spawn(Camera2dBundle {
@@ -274,7 +273,9 @@ fn setup_system(
         next_map: None,
         loaded_maps: HashSet::default(),
         entity_visibility: HashMap::default(),
+
         default_blue: default_blue.clone(),
+        default_red: default_red.clone(),
         button_color: materials.add(Color::rgb(0.4, 0.4, 0.9).into()),
         button_hovered_color: materials.add(Color::rgb(0.5, 0.5, 1.0).into()),
         button_pressed_color: materials.add(Color::rgb(0.3, 0.3, 0.8).into()),
@@ -294,15 +295,12 @@ pub fn load_next_map(
         if *map_owner != transient_state.current_map {
             transient_state.entity_visibility.insert(entity.clone(), visible.is_visible);
             visible.is_visible = false;
-            commands.remove_one::<Debuggable>(entity);
         } else {
             let is_visible = transient_state.entity_visibility.get(&entity).unwrap_or(&false);
             // ^ should default object.visible if object
             visible.is_visible = *is_visible;
         }
     }
-    // todo: add back debuggable when map made visible,
-
     // don't spawn if map already exists
     if transient_state.loaded_maps.contains(&transient_state.current_map) {
         return;
@@ -588,7 +586,7 @@ fn setup_players_system(
                 .with(Debuggable::default());
                 // Center debug indicator.
                 parent.spawn(SpriteBundle {
-                    material: default_red.clone(),
+                    material: transient_state.default_red.clone(),
                     // Don't scale here since the whole character will be scaled.
                     sprite: Sprite::new(Vec2::new(5.0, 5.0)),
                     transform: Transform::from_translation(Vec3::new(0.0, 0.0, 100.0)),
